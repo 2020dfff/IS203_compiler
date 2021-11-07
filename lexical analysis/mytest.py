feacf37f105b29e764b24e727a199079594d15d4
2020dfff <1374490218@qq.com>
2021-11-07 22:49:28 +0800

    11.7

--- lexical analysis/mytest.py
+++ lexical analysis/mytest.py
@@ -7,7 +7,7 @@ os.system("rm -rf ./mytest_results/*")
 for i, test_sample in enumerate(test_samples):
     test_sample2 = os.path.join(test_samples_path, test_sample)
     out_sample = os.path.join(out_path, test_sample.split(".")[0]+".out")
-    sen = "./little_lexer < {} > {}".format(test_sample2, out_sample)
+    sen = "./test < {} > {}".format(test_sample2, out_sample)
     os.system(sen)
     print(sen)
     print(test_sample,"ok!")
